435220f444491e5dea9ccf9ed913a7ece792e569
codediodeio <hello@codediode.io>
2018-08-14 19:12:54 -0700

    full working demo

---
+++ README.md
@@ -0,0 +1,7 @@
+## Episode 128 - Custom Theme Generator in Ionic 4
+
+Learn how to generate custom themes on the fly with Ionic 4 and CSS variables.
+
+Watch the [screencast](https://angularfirebase.com/lessons/)
+
+![Ionic 4 CSS variable tutorial](https://u5xzz.app.goo.gl/adcj)
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,46 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../theme.service';
+
+const themes = {
+  autumn: {
+    primary: '#F78154',
+    secondary: '#4D9078',
+    tertiary: '#B4436C',
+    light: '#FDE8DF',
+    medium: '#FCD0A2',
+    dark: '#B89876'
+  },
+  night: {
+    primary: '#8CBA80',
+    secondary: '#FCFF6C',
+    tertiary: '#FE5F55',
+    medium: '#BCC2C7',
+    dark: '#F7F7FF',
+    light: '#495867'
+  },
+  neon: {
+    primary: '#39BFBD',
+    secondary: '#4CE0B3',
+    tertiary: '#FF5E79',
+    light: '#F4EDF2',
+    medium: '#B682A5',
+    dark: '#34162A'
+  }
+};
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+  constructor(private theme: ThemeService) {}
+
+  changeTheme(name) {
+    this.theme.setTheme(themes[name]);
+  }
+
+  changeSpeed(val) {
+    this.theme.setVariable('--speed', `${val}ms`);
+  }
+}
